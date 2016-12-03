4b825d16e2bff217f6f630a2195fee166439146f
jonmichaeljones <jones.jonmichael@gmail.com>
2016-12-03 16:18:32 +0000

    app api content

---
+++ event_app/app_client/app.js
@@ -0,0 +1,51 @@
+(function () {
+
+  angular.module('eventApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
+
+  function config ($routeProvider, $locationProvider) {
+    
+    console.log("TEST");
+    
+    $routeProvider
+      .when('/', {
+        templateUrl: '/home/home.view.html',
+        controller: 'homeCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/directions/', {
+        templateUrl: '/directions/directions.view.html',
+        controller: 'directionsCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/details/', {
+        templateUrl: '/details/details.view.html',
+        controller: 'detailsCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/city/', {
+        templateUrl: '/city/city.view.html',
+        controller: 'cityCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/weather/', {
+        templateUrl: '/weather/weather.view.html',
+        controller: 'weatherCtrl',
+        controllerAs: 'vm'
+      })      
+      .otherwise({redirectTo: '/'});
+
+    // use the HTML5 History API
+    $locationProvider.html5Mode(
+      {
+        enabled: true,
+        requireBase: false,
+        rewriteLinks: true
+      }
+    );
+  }
+
+  angular
+    .module('eventApp')
+    .config(['$routeProvider', '$locationProvider', config]);
+
+})();
