0361be54d3742a8ebaaed2e643f850d5e31f71e7
jonmichaeljones <jones.jonmichael@gmail.com>
2016-12-03 18:13:05 +0000

    home app_client_content

---
+++ event_app/app_client/home/home.controller.js
@@ -0,0 +1,29 @@
+(function() {
+
+  angular
+    .module('eventApp')
+    .controller('homeCtrl', homeCtrl);
+
+  homeCtrl.$inject = ['$scope', `SelectedData`];
+
+  function homeCtrl($scope, SelectedData) {
+    // Nasty IE9 redirect hack (not recommended)
+    /*
+    if (window.location.pathname !== '/') {
+      window.location.href = '/#' + window.location.pathname;
+    }*/
+    var vm = this;
+    console.log(window.location);
+    
+    vm.content = "Airplane Data";
+    
+    vm.selectedCity = "";
+    
+    
+    
+    if(SelectedData.selectedCity !== null){
+      vm.selectedCity = SelectedData.selectedCity;
+    }
+  }
+
+})();
