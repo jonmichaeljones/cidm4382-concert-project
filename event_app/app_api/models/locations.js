ee5e864f21cb2d909123e0e4976e34fa94283488
jonmichaeljones <jones.jonmichael@gmail.com>
2016-12-03 17:26:25 +0000

    city app_client content

--- event_app/app_api/models/locations.js
+++ event_app/app_api/models/locations.js
@@ -1,12 +1,11 @@
 var mongoose = require('mongoose');
 
 var locationSchema = new mongoose.Schema({
-    name: String,
-    address: String,
+    cityAddress: String,
     city: String,
     state: String,
-    lat: Number,
-    long: Number
+    cityLat: Number,
+    cityLong: Number
     
 });
 
